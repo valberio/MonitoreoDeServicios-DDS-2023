87e34bc3bf7ad9d1d9d5aebaa0482faa282dcc0a
Mica Gobbi <mgobbi@frba.utn.edu.ar>
2023-06-06 09:40:13 -0300

    Test que no funcaba arafue

--- src/test/java/tests/domain/api/APITest.java
+++ src/test/java/tests/domain/api/APITest.java
@@ -25,35 +25,7 @@ public class APITest {
 
     public APITest() throws IOException {
     }
-
-
-    @Test
-    public void localizacionProvincia() throws IOException{
-
-        ServicioGeoref servicioGeoref = ServicioGeoref.getInstancia();
-        ListadoDeProvincias listadoDeProvincias = servicioGeoref.listadoDeProvincias(6);
-        ListadoDeDepartamentos listadoDeDepartamentos = servicioGeoref.listadoDeDepartamentos(5);
-        ListadoDeMunicipios listadoDeMunicipios = servicioGeoref.listadoDeMunicipios(10);
-
-
-        /*Entidad entidad1 = new Entidad("Linea Mitre", null, tipoEntidad);
-        Entidad entidad2 = new Entidad("Linea Otro", null, tipoEntidad);
-        Entidad entidad3 = new Entidad("Linea No Se",null, tipoEntidad);*/
-
-        Establecimiento establecimiento1 = new Establecimiento("Estación de Banfield");
-        Establecimiento establecimiento2 = new Establecimiento("Estación de Lanús");
-        Establecimiento establecimiento3 = new Establecimiento("Estación de Lomas de Zamora");
-
-        Provincia bsas = new Provincia();
-        bsas.obtenerse(6);
-        System.out.println(bsas.obtenerNombre(6));
-        establecimiento1.setUbicacionGeografica(bsas);
-        System.out.println(establecimiento1.getUbicacionGeografica().obtenerNombre(6));
-        /*establecimiento1.setUbicacionGeografica(listadoDeProvincias.provincias.stream().findFirst().get());
-        establecimiento2.setUbicacionGeografica(listadoDeDepartamentos.departamentos.stream().findFirst().get());
-        establecimiento3.setUbicacionGeografica(listadoDeMunicipios.municipios.stream().findFirst().get());*/
-
-    }
+    
 
     @Test
     public void probarLlamadasMunicipios() throws IOException {
