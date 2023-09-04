f7677306e6ba44fee57d5644e82c979b342adfdc
Valen <valberio@frba.utn.edu.ar>
2023-09-04 20:03:33 -0300

    Trazabilidad al cierre/apertura de incidentes

--- src/main/java/domain/incidentes/EstadoIncidente.java
+++ src/main/java/domain/incidentes/EstadoIncidente.java
@@ -1,6 +1,17 @@
 package domain.incidentes;
 
-public enum EstadoIncidente {
-    ACTIVO,
-    RESUELTO
+import domain.registro.Usuario;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+@Getter
+@Setter
+//Esto se tiene que persistir para tener trazabilidad en los estados de los incidentes
+public class EstadoIncidente {
+    Usuario usuarioResponsable;
+    LocalDateTime fechaModificacion;
+    Estado estado;
+    Incidente incidente;
+
 }
