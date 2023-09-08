41b1cb82c1c8b386a80f073995f85e6a65dfdbcb
Mica Gobbi <mgobbi@frba.utn.edu.ar>
2023-09-08 20:56:26 -0300

    testeando ranking de tiempo de cierre ( funciona :dancer: )

--- src/test/java/tests/domain/ranking/RankingPromedioTest.java
+++ src/test/java/tests/domain/ranking/RankingPromedioTest.java
@@ -23,103 +23,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-<<<<<<< Updated upstream
-=======
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-/*
->>>>>>> Stashed changes
-public class RankingPromedioTest {
-
-    @Test
-    public void testRankingPorPromedioDeCierre() throws MessagingException {
-
-        PromedioDeCierre rankingpromedioDeCierre = new PromedioDeCierre();
-
-        PreferenciaEnvioNotificacion pref = new PreferenciaEnvioNotificacion(new WhatsApp(), ModoRecepcion.SINCRONICA);
-        Usuario usuario = new Usuario("Pepita", null, null);
-        usuario.setPreferencias(pref);
-        RepositorioUsuarios.agregarUnUsuario(usuario);
-
-        LocalDateTime fechaDeReporte = LocalDateTime.of(2023, 10, 7, 7, 0);
-
-
-        Servicio baños = new Servicio("Baños", "Baños de hombres y mujeres");
-
-        Establecimiento sedeLugano = new Establecimiento("Sede Lugano", null);
-        Establecimiento sedeMedrano = new Establecimiento("Sede Medrano", null);
-
-        Entidad UTN = new Entidad();
-        UTN.setNombre("UTN");
-        UTN.agregarEstablecimientos(sedeLugano, sedeMedrano);
-        sedeLugano.setEntidad(UTN);
-        sedeMedrano.setEntidad(UTN);
-        usuario.agregarEntidadesDeInteres(UTN);
-
-        PrestacionDeServicio prestacion = new PrestacionDeServicio(baños, sedeLugano);
-        PrestacionDeServicio prestacion2 = new PrestacionDeServicio(baños, sedeMedrano);
-
-        Incidente incidente1 = new Incidente(prestacion, null,  null, null);
-        LocalDateTime tiempoAntes = LocalDateTime.of(2023, 10, 7, 7, 0);
-        incidente1.setFechaReporte(fechaDeReporte);
-
-        Incidente incidente2 = new Incidente(prestacion2, null, null, null);
-        LocalDateTime tiempoDespues = LocalDateTime.of(2023, 10, 7, 8, 0);
-        incidente2.setFechaReporte(fechaDeReporte);
-
-        incidente1.cerrarse(usuario);
-        EstadoIncidente estadoResuelto = new EstadoIncidente();
-        estadoResuelto.setUsuarioResponsable(usuario);
-        estadoResuelto.setFechaModificacion(tiempoAntes);
-        estadoResuelto.setEstado(Estado.RESUELTO);
-        incidente1.setEstado(estadoResuelto);
-        incidente1.setFechaResolucion(tiempoAntes);
-
-        incidente2.cerrarse(usuario);
-        EstadoIncidente estadoResuelto2 = new EstadoIncidente();
-        estadoResuelto2.setUsuarioResponsable(usuario);
-        estadoResuelto2.setFechaModificacion(tiempoDespues);
-        estadoResuelto2.setEstado(Estado.RESUELTO);
-        incidente2.setEstado(estadoResuelto2);
-        incidente2.setFechaResolucion(tiempoDespues);
-
-
-        Establecimiento facultadIng = new Establecimiento("Facultad de Ingeniería", null);
-        Entidad UBA = new Entidad();
-        UBA.setNombre("UBA");
-        UBA.agregarEstablecimientos(facultadIng);
-        facultadIng.setEntidad(UBA);
-        usuario.agregarEntidadesDeInteres(UBA);
-
-        PrestacionDeServicio prestacion3 = new PrestacionDeServicio(baños, facultadIng);
-        Incidente incidente3 = new Incidente(prestacion3, null,null, null);
-        incidente3.setFechaReporte(fechaDeReporte);
-
-        LocalDateTime tiempoMuchoDespues =  LocalDateTime.of(2024, 10, 7, 7, 0);
-        incidente3.cerrarse(usuario);
-        EstadoIncidente estadoResuelto3 = new EstadoIncidente();
-        estadoResuelto3.setUsuarioResponsable(usuario);
-        estadoResuelto3.setFechaModificacion(tiempoMuchoDespues);
-        estadoResuelto3.setEstado(Estado.RESUELTO);
-        incidente3.setEstado(estadoResuelto3);
-        incidente3.setFechaResolucion(tiempoMuchoDespues);
-
-        System.out.println(incidente3.obtenerEntidad().getNombre());
-        ArrayList<Entidad> resultadoRanking = rankingpromedioDeCierre.generarRanking();
-
-        resultadoRanking.forEach(resultado -> {System.out.println(resultado.getPromedioCierre()); System.out.println(resultado.getNombre());});
-
-        List<Incidente> incUBA = new ArrayList<Incidente>();
-        incUBA.add(incidente3);
-        Map<Entidad, List<Incidente>> mapa = Map.of(UBA, incUBA);
-        Map<Entidad, Double> mapaRanking = rankingpromedioDeCierre.generarPromedioCierrePorEntidad(mapa);
-        System.out.println(mapaRanking.get(UBA));
-
-}
-}
-<<<<<<< Updated upstream
-=======
-*/
 
 public class RankingPromedioTest {
 
@@ -187,4 +91,4 @@ public class RankingPromedioTest {
 
     }
 }
->>>>>>> Stashed changes
+
