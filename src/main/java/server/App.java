467796028e6f762b0df03d11125b172ec1232421
Msoteras <msoteras@frba.utn.edu.ar>
2023-12-02 19:52:48 -0300

    porfavorfunciona

--- src/main/java/server/App.java
+++ src/main/java/server/App.java
@@ -12,9 +12,7 @@ public class App {
 
 
     public static void main(String[] args) throws Exception {
-
-        Map<String, String> env = System.getenv();
-
+        
         entityManagerFactory =  createEntityManagerFactory();
 
         Server.init();
@@ -23,45 +21,18 @@ public class App {
     public static EntityManagerFactory createEntityManagerFactory() throws Exception {
         // https://stackoverflow.com/questions/8836834/read-environment-variables-in-persistence-xml-file
         Map<String, String> env = System.getenv();
-        Map<String, Object> configOverrides = new HashMap<>();
+        Map<String, Object> configOverrides = new HashMap<String, Object>();
 
-        String[] keys = new String[] {
-                "DATABASE_URL",
-                "javax__persistence__jdbc__driver",
-                "javax__persistence__jdbc__password",
-                "javax__persistence__jdbc__url",
-                "javax__persistence__jdbc__user",
-                "hibernate__hbm2ddl__auto",
-                "hibernate__connection__pool_size",
-                "hibernate__show_sql" };
+        String[] keys = new String[] { "javax.persistence.jdbc.url", "javax.persistence.jdbc.user",
+                "javax.persistence.jdbc.password", "javax.persistence.jdbc.driver", "hibernate.hbm2ddl.auto",
+                "hibernate.dialect"};
 
         for (String key : keys) {
+            if (env.containsKey(key)) {
 
-            try{
-                if (key.equals("DATABASE_URL")) {
+                String value = env.get(key);
+                configOverrides.put(key, value);
 
-                    // https://devcenter.heroku.com/articles/connecting-heroku-postgres#connecting-in-java
-                    String value = env.get(key);
-                    URI dbUri = new URI(value);
-                    String username = dbUri.getUserInfo().split(":")[0];
-                    String password = dbUri.getUserInfo().split(":")[1];
-                    //javax.persistence.jdbc.url=jdbc:postgresql://localhost/dblibros
-                    //value = "jdbc:mysql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();// + "?sslmode=require";
-                    value = "jdbc:postgres://dpg-cll2k16aov6s73f1ne0g-a.oregon-postgres.render.com/dbtpama";
-                    configOverrides.put("javax.persistence.jdbc.url", value);
-                    configOverrides.put("javax.persistence.jdbc.user", username);
-                    configOverrides.put("javax.persistence.jdbc.password", password);
-                    configOverrides.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
-                    //  configOverrides.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-                }
-                // no se pueden poner variables de entorno con "." en la key
-                String key2 = key.replace("__",".");
-                if (env.containsKey(key)) {
-                    String value = env.get(key);
-                    configOverrides.put(key2, value);
-                }
-            } catch(Exception ex){
-                System.out.println("Error configurando " + key);
             }
         }
 
